72090ea6c56b74b7da452cdf685b36135cba2325
pmartens <pieter@cg.nl>
2013-08-07 23:23:30 +0200

    adding deploy env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,17 +1,17 @@
 Spreedemo::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -34,4 +34,12 @@ Spreedemo::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = false
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  #config.assets.initialize_on_precompile = false
 end
